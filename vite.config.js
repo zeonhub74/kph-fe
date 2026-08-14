5e2845c85225e3cc5c5a8280dfb2c0261d5d86ed
starboost74 <purplecitiez@gmail.com>
2026-08-14 18:15:40 +0800

    env fix2

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://kph-be.vercel.app',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 })
